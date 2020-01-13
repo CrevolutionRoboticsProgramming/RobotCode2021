d3a291482e0a0a9e21113289f0014607d05471cf
Parker-Crevo2851 <Parkertheparker@gmail.com>
2020-01-13 18:12:59 -0500

    Drivetrain

--- src/main/java/org/frc2851/robot/subsystems/Drivetrain.java
+++ src/main/java/org/frc2851/robot/subsystems/Drivetrain.java
@@ -1,4 +1,26 @@
 package org.frc2851.robot.subsystems;
 
-public class Drivetrain {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Drivetrain extends SubsystemBase {
+    TalonSRX talon1 = new TalonSRX(0);
+    TalonSRX talon2 = new TalonSRX(1);
+    TalonSRX talon3 = new TalonSRX(2);
+    TalonSRX talon4 = new TalonSRX(3);
+
+    Joystick stick = new Joystick(0);
+
+    public Drivetrain()
+    {
+        super();
+        talon1.set(ControlMode.PercentOutput, stick.getRawAxis(1) * .5);
+        talon2.set(ControlMode.PercentOutput, stick.getRawAxis(1) * .5);
+        talon3.set(ControlMode.PercentOutput, stick.getRawAxis(5) * .5);
+        talon4.set(ControlMode.PercentOutput, stick.getRawAxis(5) * .5);
+    }
 }
