5d4d3116bc788f1a1b188f2a7aa6c2551ae12174
RyanBoring <boring.ryan.alan@gmail.com>
2020-01-17 12:41:52 -0500

    Add BadLog and rework game data handling

--- src/main/java/org/frc2851/robot/Constants.java
+++ src/main/java/org/frc2851/robot/Constants.java
@@ -10,6 +10,6 @@ public final class Constants
     public static final Controller operatorController = new Controller(1);
 
     public static final UDPHandler udpHandler = new UDPHandler(1184);
-    public static String driverStationIP = "0.0.0.0";
+    public static String driverStationIP = "";
     public static final int sendPort = 1182;
 }
