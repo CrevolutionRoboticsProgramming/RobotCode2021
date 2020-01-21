4fbe6795e2683e138f349723691831e9f300bb56
Parker-Crevo2851 <Parkertheparker@gmail.com>
2020-01-20 19:10:41 -0500

    Intake setup

---
+++ src/main/java/org/frc2851/robot/subsystems/Intake.java
@@ -0,0 +1,40 @@
+package org.frc2851.robot.subsystems;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import org.frc2851.robot.Constants;
+import org.frc2851.robot.framework.Component;
+import org.frc2851.robot.framework.Subsystem;
+import org.frc2851.robot.framework.command.CommandScheduler;
+import org.frc2851.robot.framework.command.InstantCommand;
+import org.frc2851.robot.framework.command.RunCommand;
+import org.frc2851.robot.util.MotorControllerFactory;
+
+import static org.frc2851.robot.Constants.intakeRollBar;
+import static org.frc2851.robot.Constants.intakeRollBarTrigger;
+
+
+public class Intake extends Subsystem {
+    private static Intake mInstance = new Intake();
+
+    private Intake() {
+        addComponents(new RollBar());
+    }
+
+    public static Intake getInstance() {
+        return mInstance;
+    }
+
+    public static class RollBar extends Component {
+        private CANSparkMax mMotor1;
+
+        public RollBar(){
+            super(Intake.class);
+            mMotor1 = MotorControllerFactory.makeSparkMax(intakeRollBar);
+        }
+        public void Bar(){
+            double In = Constants.driverController.get(Constants.intakeRollBarTrigger);
+        }
+    }
+}
