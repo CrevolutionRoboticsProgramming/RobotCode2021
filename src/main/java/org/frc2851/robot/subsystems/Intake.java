3af0184738781498511e8b390c658271b5f9ff1c
RyanBoring <boring.ryan.alan@gmail.com>
2020-01-25 13:15:00 -0500

    Refactor Intake

--- src/main/java/org/frc2851/robot/subsystems/Intake.java
+++ src/main/java/org/frc2851/robot/subsystems/Intake.java
@@ -1,6 +1,7 @@
 package org.frc2851.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import org.frc2851.robot.Constants;
 import org.frc2851.robot.framework.Component;
@@ -10,63 +11,67 @@ import org.frc2851.robot.framework.command.InstantCommand;
 import org.frc2851.robot.framework.command.RunCommand;
 import org.frc2851.robot.util.MotorControllerFactory;
 
-import static org.frc2851.robot.Constants.*;
-
-
-public class Intake extends Subsystem {
-
+public class Intake extends Subsystem
+{
     private static Intake mInstance = new Intake();
 
-    private Intake() {
-        addComponents(new RollBar(), new Actuate());
+    private Intake()
+    {
+        addComponents(new RollBar(), new Extender());
     }
 
-    public static Intake getInstance() {
+    public static Intake getInstance()
+    {
         return mInstance;
     }
 
-        public static class RollBar extends Component {
+    public static class RollBar extends Component
+    {
+        private VictorSPX mMotor;
 
-        private CANSparkMax mMotor1;
-
-        public RollBar() {
-
+        public RollBar()
+        {
             super(Intake.class);
 
-            mMotor1 = MotorControllerFactory.makeSparkMax(intakeRollBar);
+            mMotor = MotorControllerFactory.makeVictorSPX(Constants.intakeMotorPort);
 
-            setDefaultCommand(new RunCommand(this::rollBar, "rollBar", this));
+            CommandScheduler.getInstance().addTrigger(Constants.intakeIntakeButton::get,
+                    new InstantCommand(this::intake, "intake", this));
+            CommandScheduler.getInstance().addTrigger(Constants.intakeOuttakeButton::get,
+                    new InstantCommand(this::outtake, "outtake", this));
+            setDefaultCommand(new RunCommand(this::stop, "stop", this));
         }
 
-        public void rollBar() {
-            boolean intake = (Constants.intakeRollBarTrigger).get();
-            boolean outtake = (Constants.outtakeRollBarTrigger).get();
-
-            if(intake){
-                mMotor1.set(.5);
-            }
-           else if(outtake){
-                mMotor1.set(-.5);
-            }
-           else{
-               mMotor1.set(0);
+        private void intake()
+        {
+            mMotor.set(ControlMode.PercentOutput, 1.0);
         }
-            }
+
+        private void outtake()
+        {
+            mMotor.set(ControlMode.PercentOutput, -1.0);
         }
-    public static class Actuate extends Component{
 
-        private DoubleSolenoid mActuate;
-
-        public Actuate() {
-
-            super(Intake.class);
-
-            mActuate = new DoubleSolenoid(Constants.intakeActuateSolenoidIn, Constants.intakeActuateSolenoidOut);
-            CommandScheduler.getInstance().addTrigger(() -> !Constants.actuateIntakeBumper.get(),
-                    new InstantCommand(() -> mActuate.set(DoubleSolenoid.Value.kForward), "out", this));
-            CommandScheduler.getInstance().addTrigger(() -> Constants.actuateIntakeBumper.get(),
-                    new InstantCommand(() -> mActuate.set(DoubleSolenoid.Value.kReverse), "in", this));
+        private void stop()
+        {
+            mMotor.set(ControlMode.PercentOutput, 0.0);
         }
     }
 
+    public static class Extender extends Component
+    {
+        private DoubleSolenoid mExtenderSolenoid;
+
+        public Extender()
+        {
+            super(Intake.class);
+
+            mExtenderSolenoid = new DoubleSolenoid(Constants.intakeExtendSolenoidForward, Constants.intakeExtendSolenoidReverse);
+
+            CommandScheduler.getInstance().addTrigger(() -> !Constants.intakeExtendButton.get(),
+                    new InstantCommand(() -> mExtenderSolenoid.set(DoubleSolenoid.Value.kForward), "extend", this));
+            CommandScheduler.getInstance().addTrigger(() -> Constants.intakeExtendButton.get(),
+                    new InstantCommand(() -> mExtenderSolenoid.set(DoubleSolenoid.Value.kReverse), "retract", this));
+        }
     }
+}
